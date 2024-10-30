86ff354bdfc14580f08e1f9a0478c249a50e5942
Anwar Andargie <anwarandargie3360@gmail.com>
2024-10-30 22:23:03 +0300

    install react-router-dom and add some routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,28 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Appointment from "./pages/Appointment";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import MyAppointment from "./pages/MyAppointment";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 function App() {
-  return <h1>Prescripto</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/appointment" element={<Appointment />} />
+      <Route path="/doctors" element={<Doctors />}>
+        <Route path="/doctors:docId" />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/myappointment" element={<MyAppointment />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
