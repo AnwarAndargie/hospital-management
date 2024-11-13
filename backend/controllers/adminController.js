ad295eb9402404c1155e9971af9de3bc44c6ab45
Anwar Andargie <anwarandargie3360@gmail.com>
2024-11-13 07:15:36 +0300

    install validator package for validation

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -22,24 +22,26 @@ const addDoctor = async (req, res) => {
     experience,
     description,
     address,
-    fee,
-    image
+    fee
   );
+  console.log(image);
   try {
-    // if (
-    //   !name ||
-    //   !password ||
-    //   !email ||
-    //   !speciality ||
-    //   !degree ||
-    //   !experience ||
-    //   !description ||
-    //   !address ||
-    //   !fee
-    // ) {
-    //   res.send({ sucess: false, msg: "Data is missing" });
-    // }
-  } catch (error) {}
+    if (
+      !name ||
+      !password ||
+      !email ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !description ||
+      !address ||
+      !fee
+    ) {
+      res.send({ sucess: false, msg: "Data is missing" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { addDoctor };
