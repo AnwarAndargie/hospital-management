31df5af8f9465e26200c8649c1920c01aed6d5f5
Anwar Andargie <anwarandargie3360@gmail.com>
2024-11-15 07:08:21 +0300

    add register user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,24 @@
 import userModel from "../models/users/userModel.js";
 import bcrypt from "bcrypt";
 
-const createUser = async (req, res) => {
+export const authUser = async (req, res) => {
   const { fullName, email, password } = req.body;
   const user = await userModel.findOne({ email });
   if (!user) {
     res.json({ sucess: false, msg: "user not found" });
   }
+};
+
+export const registerUser = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (user) {
+    res.json({ sucess: false, msg: "User already exists" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  const newUser = new userModel({ fullName, email, hashedPass });
+  newUser.save();
 };
